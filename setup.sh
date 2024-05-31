0a03c67f3b70ba3c2d8a0b2d48f7b0ab1e2a271e
Tay Jovan <jovantay521@gmail.com>
2024-05-31 19:27:25 +0800

    updated setup script

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# update and upgrade packages
+sudo apt update && sudo apt full-upgrade -y
+
+# install packages
+sudo apt install xorg curl vim chromium stow lxappearance materia-gtk-theme xdg-user-dirs i3 rofi thunar qterminal kali-linux-default kali-themes -y 
+
+# stow dotfiles
+cd $HOME/Repositories/kali/dotfiles
+stow -t $HOME *
+
+# make user directories
+xdg-user-dirs-update
